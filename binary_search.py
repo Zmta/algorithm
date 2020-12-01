ba7b732bd79e74d27008d6b5a014cdf2b3ab5dbd
Zmta <Zmta@users.noreply.github.com>
2020-12-01 14:14:43 +0800

    二分查找算法-python3

--- binary_search.py
+++ binary_search.py
@@ -31,24 +31,12 @@ def binary_search(list,item):
             count = count + 1
             low=mid+1
     return None
+'''
+算法应用
 a=[]
 for i in range(128):
     a.append(i+4)
 print (a)
 print(binary_search(a,4))
+'''
 
-
-
-def binary_search(arr,item):
-    low=0
-    high=len(arr)-1
-    while low<=high:
-        mid=(low +high)//2
-        guess=arr[mid]
-        if guess==item:
-            return mid
-        if guess>item:
-            high=mid-1
-        if guess<item:
-            low=mid+1
-    return None
